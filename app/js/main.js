1925f8b57d86c6d4f589134afc387e793d38ab45
Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
2015-11-16 13:21:11 -0800

    Svg stuff

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function(){
 
-    $('.hero-heading').fitText(1);
+    // $('.hero-heading').fitText(1);
 
     var waypoints = $('.animate').waypoint({
         handler: function(direction) {
-            $(this.element).addClass('animating');
+            $(this.element).addClass('animating').removeClass('animate');
         },
         offset: '100%'
   });
