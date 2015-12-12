732b4773386acee45086125b9eca01536075e7f5
Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
2015-12-12 14:11:20 -0800

    Merging dev into master branch
    Squashed commit of the following:
    
    2eb80781a9c05abd8cc5c0af9164838a6b57d1bb
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Sat Dec 12 11:20:00 2015 -0800
    
        Fixing contact page
    
    commit 88af49f4a25fd155b74a07c6ee22a19fac48252f
    Merge: aa2f1ca ebaf87d
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Fri Dec 11 23:49:13 2015 -0800
    
        Merge branch 'development' of https://github.com/aakjaer/aakjaer.co into development
    
        # Conflicts:
        #	_layouts/default.html
    
    commit aa2f1ca267d089d9e38acd4e538e1997fd08e1b7
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Fri Dec 11 23:32:46 2015 -0800
    
        Trying to not async load typekit fonts
    
    commit ebaf87d55a0ea174ba7fafbe5fd55240217bf88f
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Fri Dec 11 23:32:46 2015 -0800
    
        Trying to not async load typekit fonts
    
    commit 6639f3e048a56b5e31a7f1435f8f344973e8f3bb
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Fri Dec 11 23:30:44 2015 -0800
    
        Playing around with animsition
    
    commit 8a5ecc0f9e1fd36f69f4a5bd9043661ec43a66ae
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Fri Dec 11 21:30:39 2015 -0800
    
        Tweaking blockquotes
    
    commit 078313e2705b203673c6233443042f340d42eb46
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Fri Dec 11 21:10:22 2015 -0800
    
        Fixing button hover/focus issue
    
    commit acff53a3f895de2df871c5430c80f2cd2aab753e
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Fri Dec 11 20:21:33 2015 -0800
    
        Tweak gallore
    
    commit 1b329185208f272ea81318f68e2ce09df7c46a59
    Author: Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
    Date:   Thu Dec 10 22:27:48 2015 -0800
    
        Updated 'siteicon.png' via CloudCannon
    
    commit 2c7f6c2f33602a0418571c2c27c1785b39424e2c
    Author: Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
    Date:   Thu Dec 10 22:21:44 2015 -0800
    
        Updated 'siteicon.png' via CloudCannon
    
    commit 2ac1e6a8741acc1226362d41a80acb522ee4479c
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Thu Dec 10 22:19:25 2015 -0800
    
        Removing unwanted separator
    
    commit 7d882363ca4cbf5fa9bb4388b0602f63f17cc1ec
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Thu Dec 10 22:13:56 2015 -0800
    
        Now we are talking
    
    commit 3ee253189c640c20349f91e016abeada18051a10
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Tue Dec 8 11:08:51 2015 -0800
    
        testing color combinations
    
    commit 222b0a02a5a3abd19f270f6d7a075882b8e84c90
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Tue Dec 8 00:20:12 2015 -0800
    
        Tweaking hero heading size
    
    commit 811dc40f89ff648caaac8e332d3456ed1e6f4eb0
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Tue Dec 8 00:18:29 2015 -0800
    
        More stuff - missing jquery
    
    commit 96151ea5e0645029095415f5d51a90fe4ee85771
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Mon Dec 7 23:28:46 2015 -0800
    
        Tweakarooo!
    
    commit ce619a2b995330ae9102af7a83db7a49f9513e30
    Author: Daniel Aakjær Sørensen <daniel@hamsterboy.dk>
    Date:   Mon Dec 7 15:05:46 2015 -0800
    
        Time to save this

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 $(function(){
 
-    var waypoints = $('.animate').waypoint({
-        handler: function(direction) {
-            $(this.element).addClass('animating').removeClass('animate');
-        },
-        offset: '90%'
+//animsition
+$('.animsition').animsition({
+    inDuration: 800,
+    outDuration: 400,
+    linkElement: '.switch-page',
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    transition: function(url){ window.location.href = url; }
   });
 
 
-  $(".fancybox").fancybox({
-      helpers: {
-          overlay: {
-            locked: false
-          }
-        }
+  // scroll related animations
+  var waypoints = $('.animate').waypoint({
+      handler: function(direction) {
+          $(this.element).addClass('animating').removeClass('animate');
+      },
+      offset: '90%'
+  });
 
+  // lightbox 
+  $(".fancybox").fancybox({
+    helpers: {
+        overlay: {
+          locked: false
+        }
+      }
   });
 });
