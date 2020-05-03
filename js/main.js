b88e3f1b36197d415eb45fdd0df2e6b486f14595
Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
2020-05-02 22:10:53 -0700

    Might be time to commit

--- js/main.js
+++ js/main.js
@@ -1,42 +1,15 @@
 $(function(){
 
-//animsition
-$('.animsition').animsition({
-    inDuration: 800,
-    outDuration: 400,
-    linkElement: '.switch-page',
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    transition: function(url){ window.location.href = url; }
-});
-
-
-  // scroll related animations
-  var waypoints = $('.animate').waypoint({
-      handler: function(direction) {
-          $(this.element).addClass('animating').removeClass('animate');
-      },
-      offset: '90%'
+  // Page preloading animation
+  $('.animsition').animsition({
+      inDuration: 800,
+      outDuration: 400,
+      linkElement: '.switch-page',
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      transition: function(url){ window.location.href = url; }
   });
-
-
-  var waypoints = $('.section--dark-gray').waypoint({
-      handler: function(direction) {
-          $('.nav-trigger').toggleClass('nav-trigger--invert', direction === 'down');
-      },
-      offset: 45
-  });
-
-  var waypoints = $('.section--dark-gray').waypoint({
-      handler: function(direction) {
-          $('.nav-trigger').toggleClass('nav-trigger--invert', direction === 'up');
-      },
-      offset: function (){
-        return -this.element.clientHeight + 45
-      }
-  });
-
 
   // lightbox
   $(".fancybox").fancybox({
@@ -46,4 +19,5 @@ $('.animsition').animsition({
         }
       }
   });
+
 });
