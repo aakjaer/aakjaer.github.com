f98b0b05a21540f2b4ee37d762f15e12363c5100
Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
2021-12-25 16:15:53 +0100

    Cleaning up

--- js/site-nav.js
+++ js/site-nav.js
@@ -23,15 +23,3 @@ jQuery(document).ready(function($){
 		}
 	}
 });
-
-
-// <script>
-//   // Look for .hamburger
-//   var hamburger = document.querySelector(".hamburger");
-//   // On click
-//   hamburger.addEventListener("click", function() {
-//     // Toggle class "is-active"
-//     hamburger.classList.toggle("is-active");
-//     // Do something else, like open/close menu
-//   });
-// </script>
