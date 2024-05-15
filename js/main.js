2a3f842000f4c468b1942089562a07a49180d952
Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
2024-05-15 18:16:16 +0200

    Make-over

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function(){
-  
+
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
   // Page preloading animation
   $('.animsition').animsition({
       inDuration: 800,
@@ -10,6 +14,7 @@ $(function(){
       onLoadEvent: true,
       transition: function(url){ window.location.href = url; }
   });
+
 
   var swiper = new Swiper(".quick-gallery", {
     effect: "fade",
@@ -22,13 +27,40 @@ $(function(){
     } 
   });
 
-    var encEmail = "ZGFuaWVsLmFha2phZXJAZ21haWwuY29t";
-    var email = "".concat(atob(encEmail));
-    
-    if ($('#encryptedEmail').html() == ""){
-      $('#encryptedEmail').html(email);
-    } 
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.hest');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-    $("#encryptedEmail").attr("href", "mailto: " + email);
+anime.timeline({loop: true})
+  .add({
+    targets: '.hest .letter',
+    duration: 2000,
+    easing: 'easeOutExpo',
+    opacity: 1,
+    rotateX: [110,0],
+    // delay: function(el, i) { return 100+index*50; },
+    // translateY: [-50,0],
+    // translateZ: 0,
+    // opacity: [0,1],
+    // easing: "easeOutExpo",
+    // duration: 1500,
+    delay: (el, i) => 100 + i * 50
+  }).add({
+      duration: 4000,
+      // delay: function(el, index) { return index*50; },
+      delay: (el, i) => i * 50,
+      easing: 'easeOutExpo',
+      opacity: 0,
+      rotateX: -110    
+  });
+
+  var encEmail = "ZGFuaWVsLmFha2phZXJAZ21haWwuY29t";
+  var email = "".concat(atob(encEmail));
+  
+  if ($('#encryptedEmail').html() == ""){
+    $('#encryptedEmail').html(email);
+  } 
+
+  $("#encryptedEmail").attr("href", "mailto: " + email);
     
 });
