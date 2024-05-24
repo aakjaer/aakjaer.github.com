1b71531bb5f258a369400521bbd157fb8e2cc259
Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
2024-05-24 10:01:51 +0200

    Fixing major mobile navigation bug

--- js/site-nav.js
+++ js/site-nav.js
@@ -1,6 +1,7 @@
-jQuery(document).ready(function($){
+$(function(){
 
-	var $rootElement = $(document.documentElement);
+	var rootElement = $(document.documentElement);
+	var burger = $(".burger");
 
 	//open/close site navigation
 	$('.nav-trigger').on('click', function(event){
@@ -8,16 +9,12 @@ jQuery(document).ready(function($){
 		toggleNav();
 	});
 
-	// burger menu
-	var $hamburger = $(".hamburger");
-	$hamburger.on("click", function(e) {
-		$hamburger.toggleClass("is-active");
-	});
-
 	function toggleNav(){
-		var navIsVisible = ( !$rootElement.hasClass('menu-shown') ) ? true : false;
-		$rootElement.toggleClass('menu-shown', navIsVisible);
-		if( !navIsVisible ) {
+		var isOpen = ( !rootElement.hasClass('menu-shown') ) ? true : false;
+		
+		rootElement.toggleClass('menu-shown', isOpen);
+		burger.toggleClass('open', isOpen);
+		if( !isOpen ) {
 			$('.site-nav').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
 			});
 		}
